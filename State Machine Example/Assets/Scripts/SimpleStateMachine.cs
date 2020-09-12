1ab216f225f6104f423f3d99086d5535d93435bc
Christian Baisch <christianbaisch@u.boisestate.edu>
2020-09-11 21:16:10 -0600

    Added working state machine

---
+++ State Machine Example/Assets/Scripts/SimpleStateMachine.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleStateMachine : MonoBehaviour
+{
+    public enum Cs { Idle, Walking};
+
+    public Transform player;
+    Animator anim;
+    public float speed;
+    public Cs currentS;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        currentS = Cs.Idle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentS == Cs.Idle)
+        {
+            Idle();
+        }
+        else if (currentS == Cs.Walking)
+        {
+            Walking();
+        }
+    }
+
+    void Idle()
+    {
+        anim.SetTrigger("isIdle");
+        //Check for exit conditions to eneter a new state
+        if (Vector3.Distance(transform.position, player.position) < 3){
+            //call the state you want to go to on this condition
+            anim.ResetTrigger("isIdle");
+            currentS = Cs.Walking;
+        }
+        //main actictivity of the state that is looping
+    }
+    void Walking()
+    {
+        anim.SetTrigger("isWalking");
+        if(Vector3.Distance(transform.position, player.position) > 4){
+            anim.ResetTrigger("isWalking");
+            currentS = Cs.Idle;
+        }
+        //main activity
+        float speedMod = speed * Time.deltaTime;
+        Vector3 playerxzPos = player.position;
+        playerxzPos.y = 0;
+        Vector3 catxzPos = transform.position;
+        catxzPos.y = 0;
+        transform.position = Vector3.Lerp(catxzPos, playerxzPos, speedMod);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.transform.position - transform.position), 1);
+    }
+}
---
+++ StateMachineGame/Assets/Scripts/AI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;  // Added since we're using a navmesh.
+
+public class AI : MonoBehaviour
+{
+    // Variables to handle what we need to send through to our state.
+    SpriteRenderer spriteRenderer;
+    public Transform player;  // To store the transform of the player. This will let the guard know where the player is, so it can face the player and know whether it should be shooting or chasing (depending on the distance).
+    State currentState;
+
+    void Start()
+    {
+        spriteRenderer = this.GetComponent<SpriteRenderer>(); // Grab agents Animator component.
+        currentState = new Idle(this.gameObject, spriteRenderer, player); // Create our first state.
+    }
+
+    void Update()
+    {
+        currentState = currentState.Process(); // Calls Process method to ensure correct state is set.
+    }
+}
