ff722db8cf420abdf74385bc73ea67855fd4fd35
RasmussenL <46792462+RasmussenL@users.noreply.github.com>
2020-09-21 13:58:08 -0600

    AnimationTest
    Set up a scene where you can switch grffin animations

---
+++ StateMachineGame/Assets/Scripts/AnimationTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour
+{
+    public Animator grif;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            grif.Play("GrifIdle");
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            grif.Play("GrifPrepSlash");
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            grif.Play("GrifSlash");
+        }
+    }
+}
