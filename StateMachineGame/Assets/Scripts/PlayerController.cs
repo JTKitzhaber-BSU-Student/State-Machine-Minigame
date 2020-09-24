6350906577a65359d2ebd38d17958f48b5857959
RasmussenL <46792462+RasmussenL@users.noreply.github.com>
2020-09-23 19:27:29 -0600

    PlayerCharacterAdded
    With funcitonality. Need to fix shooting animation while walking.

--- StateMachineGame/Assets/Scripts/PlayerController.cs
+++ StateMachineGame/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,8 @@ public class PlayerController : MonoBehaviour
     public GameObject particle;
     public Camera camera;
 
+    public Animator anim;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -20,10 +22,31 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Fire();
+        Look();
     }
 
     void FixedUpdate(){
         rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        
+    }
+
+    void Look()
+    {
+        //Get the Screen positions of the object
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+
+        //Get the Screen position of the mouse
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        //Get the angle between the points
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+        //Ta Daaa
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle + 90));
+    }
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
     }
 
     void Fire()
@@ -31,15 +54,20 @@ public class PlayerController : MonoBehaviour
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveVelocity = moveInput.normalized * speed;
 
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+                anim.Play("PlayerWalk");
+        }
+
         if (Input.GetButtonDown("Fire1"))
         {
+            anim.Play("PlayerShoot");
             GameObject clone;
             clone = Instantiate(particle, transform.position, transform.rotation);
             Vector2 mousePos = Input.mousePosition;
             mousePos = camera.ScreenToWorldPoint(mousePos);
             float angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x);
             clone.GetComponent<Rigidbody2D>().AddForce(new Vector2(Mathf.Cos(angle) * 1000f, Mathf.Sin(angle) * 1000f));
-
         }
     }
 }
