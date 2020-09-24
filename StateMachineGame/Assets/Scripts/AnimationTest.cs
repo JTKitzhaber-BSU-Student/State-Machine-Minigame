91a9cfa698997003f105133a44e5c5bcc054d3dc
RasmussenL <46792462+RasmussenL@users.noreply.github.com>
2020-09-23 20:51:23 -0600

    WolfController
    Made animations and controller for wolf sprites

--- StateMachineGame/Assets/Scripts/AnimationTest.cs
+++ StateMachineGame/Assets/Scripts/AnimationTest.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AnimationTest : MonoBehaviour
 {
     public Animator grif;
+    public Animator wolf;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,18 +18,22 @@ public class AnimationTest : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Q))
         {
             grif.Play("GrifIdle");
+            wolf.Play("WolfIdle");
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
             grif.Play("GrifPrepSlash");
+            wolf.Play("WolfPrep");
         }
         if (Input.GetKeyDown(KeyCode.E))
         {
             grif.Play("GrifSlash");
+            wolf.Play("WolfAttack");
         }
         if (Input.GetKeyDown(KeyCode.R))
         {
             grif.Play("GrifPrepWing");
+            wolf.Play("WolfSpin");
         }
         if (Input.GetKeyDown(KeyCode.T))
         {
