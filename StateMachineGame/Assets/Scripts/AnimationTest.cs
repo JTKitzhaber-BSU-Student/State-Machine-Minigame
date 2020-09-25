ba8a861faac8283ca9ae57095cc6ee9b81da7a87
RasmussenL <46792462+RasmussenL@users.noreply.github.com>
2020-09-24 20:41:18 -0600

    HydraController
    Made the hydra sprites into animations and added the arrow sprite.

--- StateMachineGame/Assets/Scripts/AnimationTest.cs
+++ StateMachineGame/Assets/Scripts/AnimationTest.cs
@@ -6,6 +6,7 @@ public class AnimationTest : MonoBehaviour
 {
     public Animator grif;
     public Animator wolf;
+    public Animator hydra;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,16 +20,19 @@ public class AnimationTest : MonoBehaviour
         {
             grif.Play("GrifIdle");
             wolf.Play("WolfIdle");
+            hydra.Play("HydraIdle");
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
             grif.Play("GrifPrepSlash");
             wolf.Play("WolfPrep");
+            hydra.Play("HydraTailAttack");
         }
         if (Input.GetKeyDown(KeyCode.E))
         {
             grif.Play("GrifSlash");
             wolf.Play("WolfAttack");
+            hydra.Play("HydraFireAttack");
         }
         if (Input.GetKeyDown(KeyCode.R))
         {
